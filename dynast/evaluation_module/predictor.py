225fa09bd46ad989d2efbf801d545b5029a694d6
Sarah, Anthony <anthony.sarah@intel.com>
2021-12-14 00:15:15 +0000

    Modified the MobileNetAccuracyPredictor class and added ResNet50AccuracyPredictor and ResNet50LatencyPredictor classes.

--- dynast/evaluation_module/predictor.py
+++ dynast/evaluation_module/predictor.py
@@ -1,6 +1,9 @@
-import numpy as np
 import pickle
-from sklearn import svm, linear_model
+
+import numpy as np
+from scipy.stats import kendalltau, spearmanr
+from sklearn import linear_model, svm
+from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error
 from sklearn.model_selection import GridSearchCV
 
 
@@ -15,14 +18,79 @@ class Predictor:
         self.regressor = self.searcher.best_estimator_
 
     def predict(self, examples):
+        '''
+        Predicts the output values of the specified examples using the underlying regressor.
 
+        Parameters
+        ----------
+            examples: Examples for which predictions will be made.
+
+        Returns
+        -------
+            Predictions of the specified examples.
+        '''
+
+        # Return predictions
         return self.regressor.predict(examples)
 
-    def predict_single(self, example):
-        
-        return self.regressor.predict(example.reshape(1,-1))[0]  
+    def get_parameters(self):
+        '''
+        Returns the optimal parameter values of the underlying regressor.
+
+        Parameters
+        ----------
+            None
+
+        Returns
+        -------
+            Optimal parameter values of the underlying regressor.
+        '''
+
+        # Retrieve optimal parameters
+        return self.searcher.best_params_
+
+    def get_metrics(self, examples, labels):
+        '''
+        Computes the performance metrics of the underlying regressor.
+
+        Parameters
+        ----------
+            examples: Examples to use when computing performance metrics.
+
+            labels: Labels to use when computing performance metrics.
+
+        Returns
+        -------
+            Performance metrics of the underlying regressor. The metrics are
+                Mean absolute percentage error (MAPE)
+                Root mean squared error (RMSE)
+                Kendall rank correlation coefficient (kendall)
+                Spearman's rank correlation coefficient (spearman)
+        '''
+
+        # Compute predictions of specified examples
+        predictions = self.predict(examples)
+
+        # Compute performance metrics
+        mape = 100.0 * mean_absolute_percentage_error(labels, predictions)
+        rmse = mean_squared_error(labels, predictions, squared=False)
+        kendall, _ = kendalltau(labels, predictions)
+        spearman, _ = spearmanr(labels, predictions)
+
+        return mape, rmse, kendall, spearman
 
     def load(self, filename):
+        '''
+        Loads the model of the underlying regressor and searcher.
+
+        Parameters
+        ----------
+            filename: Name of the file from which to load the model.
+
+        Returns
+        -------
+            None
+        '''
 
         # Load searcher and regressor from specified file
         with open(filename, 'rb') as input_file:
@@ -30,6 +98,17 @@ class Predictor:
             self.regressor = pickle.load(input_file)
 
     def save(self, filename):
+        '''
+        Saves the model of the underlying regressor and searcher.
+
+        Parameters
+        ----------
+            filename: Name of the file to which to save the model.
+
+        Returns
+        -------
+            None
+        '''
 
         # Save searcher and regressor to specified file
         with open(filename, 'wb') as output_file:
@@ -44,12 +123,12 @@ class RidgePredictor(Predictor):
         SEARCHER_VERBOSITY = 10
 
         # Create regressor
-        self.regressor = linear_model.Ridge(max_iter = max_iterations)
+        self.regressor = linear_model.Ridge(max_iter=max_iterations)
 
         # Create parameter searcher
         search_parameters = {'alpha': alphas}
-        self.searcher = GridSearchCV(estimator = self.regressor, param_grid = search_parameters, n_jobs = -1,
-            scoring = 'neg_root_mean_squared_error', verbose = SEARCHER_VERBOSITY if (verbose) else 0)
+        self.searcher = GridSearchCV(estimator=self.regressor, param_grid=search_parameters, n_jobs=-1,
+                                     scoring='neg_root_mean_squared_error', verbose=SEARCHER_VERBOSITY if (verbose) else 0)
 
 
 class SVRPredictor(Predictor):
@@ -61,14 +140,14 @@ class SVRPredictor(Predictor):
         # Create regressor
         self.regressor = None
         if (kernel_type == 'linear'):
-            self.regressor = svm.LinearSVR(max_iter = max_iterations)
+            self.regressor = svm.LinearSVR(max_iter=max_iterations)
         else:
-            self.regressor = svm.SVR(kernel = kernel_type, max_iter = max_iterations)
+            self.regressor = svm.SVR(kernel=kernel_type, max_iter=max_iterations)
 
         # Create parameter searcher
         search_parameters = {'C': cost_factors, 'epsilon': epsilons}
-        self.searcher = GridSearchCV(estimator = self.regressor, param_grid = search_parameters, n_jobs = -1,
-            scoring = 'neg_root_mean_squared_error', verbose = SEARCHER_VERBOSITY if (verbose) else 0)
+        self.searcher = GridSearchCV(estimator=self.regressor, param_grid=search_parameters, n_jobs=-1,
+                                     scoring='neg_root_mean_squared_error', verbose=SEARCHER_VERBOSITY if (verbose) else 0)
 
 
 class ResNet50AccuracyPredictor(RidgePredictor):
@@ -76,7 +155,7 @@ class ResNet50AccuracyPredictor(RidgePredictor):
     DEFAULT_ALPHAS = np.arange(0.5, 2.5, 0.5)
     DEFAULT_MAX_ITERATIONS = 1000000
 
-    def __init__(self, alphas = DEFAULT_ALPHAS, max_iterations = DEFAULT_MAX_ITERATIONS, verbose = False):
+    def __init__(self, alphas=DEFAULT_ALPHAS, max_iterations=DEFAULT_MAX_ITERATIONS, verbose=False):
 
         super().__init__(alphas, max_iterations, verbose)
 
@@ -86,7 +165,7 @@ class ResNet50LatencyPredictor(RidgePredictor):
     DEFAULT_ALPHAS = np.arange(0.5, 2.5, 0.5)
     DEFAULT_MAX_ITERATIONS = 1000000
 
-    def __init__(self, alphas = DEFAULT_ALPHAS, max_iterations = DEFAULT_MAX_ITERATIONS, verbose = False):
+    def __init__(self, alphas=DEFAULT_ALPHAS, max_iterations=DEFAULT_MAX_ITERATIONS, verbose=False):
 
         super().__init__(alphas, max_iterations, verbose)
 
@@ -96,7 +175,7 @@ class MobileNetAccuracyPredictor(RidgePredictor):
     DEFAULT_ALPHAS = np.arange(0.5, 2.5, 0.5)
     DEFAULT_MAX_ITERATIONS = 1000000
 
-    def __init__(self, alphas = DEFAULT_ALPHAS, max_iterations = DEFAULT_MAX_ITERATIONS, verbose = False):
+    def __init__(self, alphas=DEFAULT_ALPHAS, max_iterations=DEFAULT_MAX_ITERATIONS, verbose=False):
 
         super().__init__(alphas, max_iterations, verbose)
 
@@ -106,7 +185,7 @@ class MobileNetLatencyPredictor(RidgePredictor):
     DEFAULT_ALPHAS = np.arange(0.5, 2.5, 0.5)
     DEFAULT_MAX_ITERATIONS = 1000000
 
-    def __init__(self, alphas = DEFAULT_ALPHAS, max_iterations = DEFAULT_MAX_ITERATIONS, verbose = False):
+    def __init__(self, alphas=DEFAULT_ALPHAS, max_iterations=DEFAULT_MAX_ITERATIONS, verbose=False):
 
         super().__init__(alphas, max_iterations, verbose)
 
@@ -116,7 +195,7 @@ class MobileNetMACsPredictor(RidgePredictor):
     DEFAULT_ALPHAS = np.arange(0.1, 0.6, 0.1)
     DEFAULT_MAX_ITERATIONS = 1000000
 
-    def __init__(self, alphas = DEFAULT_ALPHAS, max_iterations = DEFAULT_MAX_ITERATIONS, verbose = False):
+    def __init__(self, alphas=DEFAULT_ALPHAS, max_iterations=DEFAULT_MAX_ITERATIONS, verbose=False):
 
         super().__init__(alphas, max_iterations, verbose)
 
@@ -127,7 +206,7 @@ class TransformerBleuPredictor(SVRPredictor):
     DEFAULT_EPSILONS = np.arange(0.0, 0.55, 0.05)
     DEFAULT_MAX_ITERATIONS = 1000000
 
-    def __init__(self, kernel_type = 'rbf', cost_factors = DEFAULT_COST_FACTORS, epsilons = DEFAULT_EPSILONS, max_iterations = DEFAULT_MAX_ITERATIONS, verbose = False):
+    def __init__(self, kernel_type='rbf', cost_factors=DEFAULT_COST_FACTORS, epsilons=DEFAULT_EPSILONS, max_iterations=DEFAULT_MAX_ITERATIONS, verbose=False):
 
         super().__init__(kernel_type, cost_factors, epsilons, max_iterations, verbose)
 
@@ -137,6 +216,6 @@ class TransformerLatencyPredictor(RidgePredictor):
     DEFAULT_ALPHAS = np.arange(5.0, 26.0, 1.0)
     DEFAULT_MAX_ITERATIONS = 1000000
 
-    def __init__(self, alphas = DEFAULT_ALPHAS, max_iterations = DEFAULT_MAX_ITERATIONS, verbose = False):
+    def __init__(self, alphas=DEFAULT_ALPHAS, max_iterations=DEFAULT_MAX_ITERATIONS, verbose=False):
 
         super().__init__(alphas, max_iterations, verbose)
