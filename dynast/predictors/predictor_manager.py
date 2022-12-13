ba547fe92553c08a81146fbe6e6c540417a5cd7d
Maciej Szankin <macsz@users.noreply.github.com>
2022-12-13 21:06:31 +0100

    Setup improvements (#87)
    * Change license to Apache 2.0
    * Make requirements less strict
    * Update setup.py script
      * Classifiers
      * Description
      * Update contributors
      * Update version
    
    Signed-off-by: Maciej Szankin <maciej.szankin@intel.com>

--- dynast/predictors/predictor_manager.py
+++ dynast/predictors/predictor_manager.py
@@ -1,15 +1,16 @@
-# INTEL CONFIDENTIAL
-# Copyright 2022 Intel Corporation. All rights reserved.
-
-# This software and the related documents are Intel copyrighted materials, and your use of them is governed by the
-# express license under which they were provided to you ("License"). Unless the License provides otherwise, you may
-# not use, modify, copy, publish, distribute, disclose or transmit this software or the related documents without
-# Intel's prior written permission.
-
-# This software and the related documents are provided as is, with no express or implied warranties, other than those
-# that are expressly stated in the License.
-
-# This software is subject to the terms and conditions entered into between the parties.
+# Copyright (c) 2022 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from dynast.predictors.dynamic_predictor import Predictor
 from dynast.utils import log
--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
-import datetime
-import subprocess
+# Copyright (c) 2022 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# import datetime
 
 from setuptools import find_packages, setup
 
 
-def get_git_hash():
-    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('UTF-8').strip()
-
-
-def get_build_name():
-    # TODO (Maciej): WW should be based on the commit date, not current date
-    return 'ww{}.{}-{}'.format(
-        datetime.datetime.utcnow().strftime("%V"), datetime.datetime.utcnow().isoweekday(), get_git_hash()
-    )
+def get_version():
+    # TODO(macsz) Replace with __version__
+    return '1.0'
 
 
 def get_dependencies():
@@ -37,27 +43,29 @@ def get_test_dependencies():
 
 setup(
     name='dynast',
-    version=get_build_name(),
+    version=get_version(),
     description='DyNAS-T (Dynamic Neural Architecture Search Toolkit) - a SuperNet NAS optimization package',
-    long_description='DyNAS-T (Dynamic Neural Architecture Search Toolkit) is a SuperNet NAS optimization package '
-    'designed for finding the optimal Pareto front during neural architure search while minimizing '
-    'the number of search validation measurements. It supports single-/multi-/many-objective '
-    'problems for a variety of domains supported by the Intel AI Lab HANDI framework. The system '
-    'currently heavily utilizes the pymoo optimization library.',
+    long_description=open("README.md", "r", encoding='utf-8').read(),
     long_description_content_type="text/markdown",
-    author='Cummings, Daniel J; Nittur Sridhar, Sharath; Sarah, Anthony; Sundaresan, Sairam; ' 'Szankin, Maciej;',
-    author_email='daniel.j.cummings@intel.com; sharath.nittur.sridhar@intel.com; anthony.sarah@intel.com; '
+    author='Nittur Sridhar, Sharath; Sarah, Anthony; Sundaresan, Sairam; Szankin, Maciej;',
+    author_email='sharath.nittur.sridhar@intel.com; anthony.sarah@intel.com; '
     'sairam.sundaresan@intel.com; maciej.szankin@intel.com;',
-    license='Intel Confidential',  # TODO(Maciej) Update license
+    license='Apache 2.0',
     packages=find_packages(),
     install_requires=get_dependencies(),
     extras_require={
         'test': get_test_dependencies(),
     },
-    zip_safe=False,
     entry_points={
         'console_scripts': [
             'dynast=dynast.cli:main',
         ],
     },
+    python_requires='>=3.6.0',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: Apache Software License',
+    ],
 )
