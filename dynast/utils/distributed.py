553b0c05504f8c4f0ec4c2636677538ed4c9a48a
Maciej Szankin <maciej.szankin@intel.com>
2023-02-15 18:22:26 -0800

    Update distributed utils
    Signed-off-by: Maciej Szankin <maciej.szankin@intel.com>

--- dynast/utils/distributed.py
+++ dynast/utils/distributed.py
@@ -4,15 +4,8 @@ from typing import Tuple
 
 import torch.distributed as dist
 
-LOCAL_RANK = None
-WORLD_RANK = None
-WORLD_SIZE = None
-DIST_METHOD = None
-
 
 def get_distributed_vars() -> Tuple[int, int, int, str]:
-    global LOCAL_RANK, WORLD_RANK, WORLD_SIZE, DIST_METHOD
-
     # if torchrun was used
     local_rank = os.getenv('LOCAL_RANK')
     world_rank = os.getenv('RANK')
@@ -20,20 +13,20 @@ def get_distributed_vars() -> Tuple[int, int, int, str]:
     dist_method = 'torchrun'
 
     # if mpi
-    if not local_rank:
+    if not world_rank:
         local_rank = os.getenv('OMPI_COMM_WORLD_LOCAL_RANK')
         world_rank = os.getenv('OMPI_COMM_WORLD_RANK')
         world_size = os.getenv('OMPI_COMM_WORLD_SIZE')
         dist_method = 'mpi'
 
-        if not local_rank:
+        if not world_rank:
             # at this point if local_rank is not set, then it's just a single process
             dist_method = None
 
     if dist_method is not None:
-        LOCAL_RANK, WORLD_RANK, WORLD_SIZE = int(local_rank), int(world_rank), int(world_size)
-    DIST_METHOD = dist_method
-    return LOCAL_RANK, WORLD_RANK, WORLD_SIZE, DIST_METHOD
+        local_rank, world_rank, world_size = int(local_rank), int(world_rank), int(world_size)
+    dist_method = dist_method
+    return local_rank, world_rank, world_size, dist_method
 
 
 def init_processes(backend: str, world_rank: int, world_size: int) -> None:
@@ -41,15 +34,17 @@ def init_processes(backend: str, world_rank: int, world_size: int) -> None:
 
 
 def is_main_process() -> bool:
-    if WORLD_RANK is None:
+    _, world_rank, _, _ = get_distributed_vars()
+    if world_rank is None:
         raise Exception('Not running in distributed mode.')
-    return WORLD_RANK == 0
+    return world_rank == 0
 
 
 def is_worker_process() -> bool:
-    if WORLD_RANK is None:
+    _, world_rank, _, _ = get_distributed_vars()
+    if world_rank is None:
         raise Exception('Not running in distributed mode.')
-    return WORLD_RANK != 0
+    return world_rank != 0
 
 
 def get_worker_results_path(results_path: str, worker_id: int) -> str:
--- tests/test_utils_distributed.py
+++ tests/test_utils_distributed.py
@@ -1,4 +1,9 @@
-from dynast.utils.distributed import WORLD_RANK, get_worker_results_path
+import os
+from unittest import mock
+
+import pytest
+
+from dynast.utils.distributed import get_distributed_vars, get_worker_results_path, is_main_process, is_worker_process
 
 
 def test_get_worker_results_path() -> None:
@@ -9,3 +14,57 @@ def test_get_worker_results_path() -> None:
     results_path = 'test.csv'
     worker_id = 2
     assert 'test_2.csv' == get_worker_results_path(results_path, worker_id)
+
+
+def test_get_distributed_vars() -> None:
+    with mock.patch.dict(os.environ, {"RANK": "2", "LOCAL_RANK": "1", "WORLD_SIZE": "2"}):
+        local_rank, world_rank, world_size, dist_method = get_distributed_vars()
+        assert local_rank == 1 and world_rank == 2 and world_size == 2 and dist_method == 'torchrun'
+
+    with mock.patch.dict(
+        os.environ, {"OMPI_COMM_WORLD_RANK": "2", "OMPI_COMM_WORLD_LOCAL_RANK": "1", "OMPI_COMM_WORLD_SIZE": "2"}
+    ):
+        local_rank, world_rank, world_size, dist_method = get_distributed_vars()
+        assert local_rank == 1 and world_rank == 2 and world_size == 2 and dist_method == 'mpi'
+
+
+def test_is_main_process() -> None:
+    with mock.patch.dict(os.environ, {"RANK": "0", "LOCAL_RANK": "0", "WORLD_SIZE": "2"}):
+        assert is_main_process()
+
+    with mock.patch.dict(os.environ, {"RANK": "1", "LOCAL_RANK": "1", "WORLD_SIZE": "2"}):
+        assert not is_main_process()
+
+    with mock.patch.dict(
+        os.environ, {"OMPI_COMM_WORLD_RANK": "0", "OMPI_COMM_WORLD_LOCAL_RANK": "0", "OMPI_COMM_WORLD_SIZE": "2"}
+    ):
+        assert is_main_process()
+
+    with mock.patch.dict(
+        os.environ, {"OMPI_COMM_WORLD_RANK": "1", "OMPI_COMM_WORLD_LOCAL_RANK": "1", "OMPI_COMM_WORLD_SIZE": "2"}
+    ):
+        assert not is_main_process()
+
+    with pytest.raises(Exception):
+        is_main_process()
+
+
+def test_is_worker_process() -> None:
+    with mock.patch.dict(os.environ, {"RANK": "0", "LOCAL_RANK": "0", "WORLD_SIZE": "2"}):
+        assert not is_worker_process()
+
+    with mock.patch.dict(os.environ, {"RANK": "1", "LOCAL_RANK": "1", "WORLD_SIZE": "2"}):
+        assert is_worker_process()
+
+    with mock.patch.dict(
+        os.environ, {"OMPI_COMM_WORLD_RANK": "0", "OMPI_COMM_WORLD_LOCAL_RANK": "0", "OMPI_COMM_WORLD_SIZE": "2"}
+    ):
+        assert not is_worker_process()
+
+    with mock.patch.dict(
+        os.environ, {"OMPI_COMM_WORLD_RANK": "1", "OMPI_COMM_WORLD_LOCAL_RANK": "1", "OMPI_COMM_WORLD_SIZE": "2"}
+    ):
+        assert is_worker_process()
+
+    with pytest.raises(Exception):
+        is_worker_process()
