1bdade1d7e378a7f00fdf9eb578579dff85e60ca
Maciej Szankin <macsz@users.noreply.github.com>
2023-03-03 14:41:20 -0800

    Pre-release checks and fixes (#11)
    * lose the requirements to make integration with Neural Compressor
    possible
    * Add missing package file
    * Fix formatting
    
    ---------
    
    Signed-off-by: Maciej Szankin <maciej.szankin@intel.com>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import find_packages, setup
 
 def get_version():
     # TODO(macsz) Replace with __version__
-    return '1.0.0'
+    return '1.1.0rc4'
 
 
 def get_dependencies():
