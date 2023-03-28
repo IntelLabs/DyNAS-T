2e06dbece47b7b1119c63a75a5fa876b79a52fb1
Maciej Szankin <maciej.szankin@intel.com>
2023-03-28 12:32:55 -0700

    Add tests scripts to run short tests on OFA MBv3 and R50 supernets
    Signed-off-by: Maciej Szankin <maciej.szankin@intel.com>

---
+++ tests/scripts/run_ofambv3_random_short.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source $( dirname -- "$0"; )/config.sh
+
+time python run_search.py \
+        --results_path ${RESULTS_PATH}/results_ofambv3_random_short.csv \
+        --supernet ofa_mbv3_d234_e346_k357_w1.0 \
+        --dataset_path  ${DATASET_IMAGENET_PATH} \
+        --search_tactic random \
+        --population ${SHORT_RANDOM_POPULATION} \
+        --valid_size ${SHORT_RANDOM_VALID_SIZE} \
+        --seed ${SEED} \
+        --measurements macs accuracy_top1 \
+        --num_evals ${SHORT_RANDOM_POPULATION}
