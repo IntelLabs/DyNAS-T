bb8e4f191af8eb9c5e367c10521292ed65585edd
Maciej Szankin <macsz@users.noreply.github.com>
2023-04-24 14:56:19 -0700

    Add verbosity option to OFA (#32)
    By default OFA prints whole model architecture. This change adds an
    option to tune verbosity in OFA and by default sets it to disabled.
    
    Signed-off-by: Maciej Szankin <maciej.szankin@intel.com>

--- dynast/search/search_tactic.py
+++ dynast/search/search_tactic.py
@@ -191,6 +191,7 @@ class NASBaseConfig:
                 device=self.device,
                 dataloader_workers=self.dataloader_workers,
                 test_size=self.test_size,
+                verbose=self.verbose,
             )
         elif self.supernet == 'transformer_lt_wmt_en_de':
             self.runner_validate = TransformerLTRunner(
@@ -347,6 +348,7 @@ class LINAS(NASBaseConfig):
                     device=self.device,
                     dataloader_workers=self.dataloader_workers,
                     test_size=self.test_size,
+                    verbose=self.verbose,
                 )
             elif self.supernet == 'transformer_lt_wmt_en_de':
                 runner_predict = TransformerLTRunner(
@@ -783,6 +785,7 @@ class LINASDistributed(LINAS):
                         device=self.device,
                         dataloader_workers=self.dataloader_workers,
                         test_size=self.test_size,
+                        verbose=self.verbose,
                     )
                 elif self.supernet == 'transformer_lt_wmt_en_de':
                     runner_predict = TransformerLTRunner(
