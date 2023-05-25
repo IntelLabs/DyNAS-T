2211a61dbfded6a2248063bd148fc2a4a7375159
Maciej Szankin <macsz@users.noreply.github.com>
2023-05-25 16:45:47 -0400

    break something

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -11,10 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import pytest
 
 import os
-
-import pytest
 
 from dynast.utils.datasets import CIFAR10, Dataset, ImageNet, Imagenette
 
