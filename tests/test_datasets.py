31296aea4e68ffba6447a347668a992683574728
Maciej Szankin <macsz@users.noreply.github.com>
2023-05-25 15:10:28 -0400

    Test black

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -42,8 +42,7 @@ def test_dataset_invalid_name_exception():
 
 
 @pytest.mark.skipif(
-    os.path.exists(not "/datasets/imagenet-ilsvrc2012/val"),
-    reason="ImageNet dataset not found; skipping test (TODO(macsz) should be mocked in the future!)",
+    os.path.exists(not "/datasets/imagenet-ilsvrc2012/val"), reason="ImageNet dataset not found; skipping test (TODO(macsz) should be mocked in the future!)",
 )
 def test_dataset_test_fraction():
     ImageNet.PATH = "/datasets/imagenet-ilsvrc2012/"
