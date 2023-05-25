0e7740e58c1a6f295046398c7fd47084ea684ce0
Maciej Szankin <macsz@users.noreply.github.com>
2023-05-25 15:05:44 -0400

    Revert isort test

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -12,10 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 
 import pytest
-
-import os
 
 from dynast.utils.datasets import CIFAR10, Dataset, ImageNet, Imagenette
 
