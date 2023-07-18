f58ecc099f7ef74aa6d3a95b7ddeae71285c39db
Maciej Szankin <maciej.szankin@intel.com>
2023-07-18 08:28:59 -0700

    Update reqs
    Signed-off-by: Maciej Szankin <maciej.szankin@intel.com>

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
     install_requires=get_dependencies(),
     extras_require={
         'test': get_test_dependencies(),
+        'plot': ['matplotlib', 'shapely', 'alphashape', 'descartes'],
     },
     entry_points={
         'console_scripts': [
