e3f5aadd0dac40a68295d5b0319632fc1e542cca
Maciej Szankin <maciej.szankin@intel.com>
2023-09-01 13:49:24 -0700

    Fix BootstrapNAS dependencies
    Signed-off-by: Maciej Szankin <maciej.szankin@intel.com>

--- setup.py
+++ setup.py
@@ -48,6 +48,19 @@ def get_dependencies(feature=None):
     for fn in fns:
         deps.extend(_read_requirements(fn))
 
+    # TODO(macsz) At the moment, PyPi will produce an error if you attempt to install NNCF from GitHub. This is because
+    # the necessary features have not yet been included in any released versions of NNCF, so it must be removed from
+    # this list and installed manually by the user.
+    # For now, to install NNCF, use the following steps:
+    # ```
+    # git clone https://github.com/openvinotoolkit/nncf.git
+    # cd nncf
+    # git checkout e0bc50359992a3d73d4ed3e6396c8b4f1d4ae631
+    # pip install -e .
+    # ```
+    # Once version 2.6 of NNCF is released, the following line can be deleted:
+    deps = [d for d in deps if 'nncf' not in d]
+
     deps = list(sorted(set(deps)))
     return deps
 
